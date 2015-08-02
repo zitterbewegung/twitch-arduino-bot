5d5fe2f0e484846a4810f41e206f440bd7f566ad
Joshua Herman <zitterbewegung@gmail.com>
2015-08-02 07:01:31 -0500

    Initial commit of prototype. Verified to work with the esplora.

---
+++ example-conf.js
@@ -0,0 +1,4 @@
+var config = {}
+/*Config.oauth must be set. Rename this file to config.js or nothing will work I have used the following website. http://twitchapps.com/tmi/ */
+config.oauth = ''
+module.exports = config;
